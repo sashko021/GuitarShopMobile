2bdb20a7f1d5c47cb5949b98ba0a40ead55cfb28
Mandingoman <how998989@gmail.com>
2025-01-14 18:37:10 +0200

    DBhelper

--- app/src/main/java/com/example/guitarshopmobile/LoginScreen.java
+++ app/src/main/java/com/example/guitarshopmobile/LoginScreen.java
@@ -1,6 +1,35 @@
 package com.example.guitarshopmobile;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class LoginScreen extends AppCompatActivity {
+
+    EditText username,password;
+
+
+    protected void onCreate (Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+
+        setContentView(R.layout.login_screen_layout);
+        AppCompatButton sign_up_btn = findViewById(R.id.signup_btn);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        sign_up_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup = new Intent("com.example.guitarshopmobile.SignUpScreen");
+                startActivity(signup);
+            }
+        });
+
+
+    }
 }
---
+++ app/src/main/java/com/example/guitarshopmobile/SQLiteHelper.java
@@ -0,0 +1,24 @@
+package com.example.guitarshopmobile;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    public SQLiteHelper(@Nullable Context context) {
+        super(context, "GuitarShop.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int version_old, int version_new) {
+
+    }
+}
