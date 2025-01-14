4b6a6f9e08e5fe90672def296b7821d5acc2e310
sashko021 <trayan4@abv.bg>
2025-01-14 22:29:16 +0200

    second commit third time

--- app/src/main/java/com/example/guitarshopmobile/ElectricGuitar.java
+++ app/src/main/java/com/example/guitarshopmobile/ElectricGuitar.java
@@ -26,9 +26,19 @@ public class ElectricGuitar extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler);
 
         List<Items> items = new ArrayList<Items>();
-        items.add(new Items(R.drawable.epiphone_es335,"Epiphone","470$"));
-        items.add(new Items(R.drawable.epiphone_es335,"Epiphone","470$"));
-        items.add(new Items(R.drawable.epiphone_es335,"Epiphone","470$"));
+        items.add(new Items(R.drawable.es335,"Epiphone-ES225\n" +
+                "Body\tLayered Maple\t" +
+                "Neck\tMahogany\t" +
+                "Neck Shape\tRounded C\t" +
+                "Fingerboard\tIndian Laurel\t" +
+                "Scale\t24.724\t" +
+                "        Frets\t22\t" +
+                "    Nut\tGraph Tech NuBone\t" +
+                "    Nut Width\t1.693\t" +
+                "    Controls\t2 x Volume, 2 x Tone, 3 Way Selector\t" +
+                "    Bridge\tTune-O-Matic","470$"));
+        items.add(new Items(R.drawable.es335_2,"Epiphone","470$"));
+        items.add(new Items(R.drawable.es335,"Epiphone","470$"));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(),items));
